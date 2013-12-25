b84748665bd1da6c2c99e27220370b60d8e85b70
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-25 16:05:48 -0500

    dev profile

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject rabble "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "forum api"
+  :url "https://github.com/flyingmachine/rabble"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]
@@ -23,4 +23,18 @@
                  [flyingmachine/cartographer "0.1.1"]
                  [com.flyingmachine/penny-black-postal "0.1.0"]
                  [com.flyingmachine/datomic-junk "0.1.3"]
-                 [com.flyingmachine/liberator-templates "0.1.1"]])
+                 [com.flyingmachine/liberator-templates "0.1.1"]]
+
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.4360"]
+                                  [midje "1.5.0"]
+                                  [ring "1.2.1"]
+                                  [ring-mock "0.1.4"]]
+                   :env {:com-flyingmachine-penny-black
+                         {:template-path "email-templates"
+                          :send-email false
+                          :from-address "notifications@rabble-forum.com"
+                          :from-name "Grateful Place Notifications"}
+                         :datomic {:db-uri "datomic:free://localhost:4334/rabble"
+                                   :test-uri "datomic:mem://rabble-test"}
+                         :rabble {:moderator-names ["flyingmachine"]}}}
+             :test [:dev]})
