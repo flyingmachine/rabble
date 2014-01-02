1282e701c3a4109ac4dea6cf87d09b87fd95366e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-02 10:25:23 -0500

    workflow reloaded

--- project.clj
+++ project.clj
@@ -25,7 +25,8 @@
                  [com.flyingmachine/datomic-junk "0.1.3"]
                  [com.flyingmachine/liberator-templates "0.1.1"]]
 
-  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.4360"]
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[com.datomic/datomic-free "0.9.4360"]
                                   [midje "1.5.0"]
                                   [ring "1.2.1"]
                                   [ring-mock "0.1.4"]]
