ecad83e70a266c0fbc97be928f8b15cc526b9249
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-07-17 15:17:37 -0400

    watch working

---
+++ src/rabble/resources/watch.clj
@@ -0,0 +1,37 @@
+(ns rabble.resources.watch
+  (:require [datomic.api :as d]
+            [rabble.db.transactions.watches :as tx]
+            [rabble.db.maprules :as mr]
+            [rabble.db.mapification :refer :all]
+            [rabble.resources.shared :refer :all]
+            [rabble.models.permissions :refer :all]
+            [rabble.email.sending.notifications :as n]
+            [flyingmachine.cartographer.core :as c]
+            [com.flyingmachine.datomic-junk :as dj]
+            [flyingmachine.webutils.utils :refer :all]))
+
+(def watch (mapifier mr/ent->watch))
+
+(defn resource-decisions
+  [options defaults app-config]
+  (merge-with
+   merge defaults
+   {:list {:handle-ok (fn [ctx]
+                        (map (comp watch first)
+                             (d/q '[:find ?watch
+                                    :in $ ?userid
+                                    :where [?watch :watch/user ?userid]
+                                    [?watch :watch/topic ?topic]
+                                    [?topic :content/deleted false]]
+                                  (dj/db)
+                                  (:id (auth ctx)))))}
+    :create {:authorized? ctx-logged-in?
+             :post! (fn [ctx] (create-record ctx tx/create-watch watch))
+             :handle-created record-in-ctx}
+    :delete {:authorized? (fn [ctx]
+                            (let [watch-id (str->int (:id (params ctx)))
+                                  watch (dj/ent watch-id)]
+                              (if (and watch (= (:db/id (:watch/user watch))
+                                                (:id (auth ctx))))
+                                {:record watch-id})))
+             :delete! (fn [ctx] (dj/retract (:record ctx)))}}))
---
+++ test/rabble/resources/watch_test.clj
@@ -0,0 +1,37 @@
+(ns rabble.resources.like-test
+  (:require [com.flyingmachine.datomic-junk :as dj]
+            [rabble.resources.watch :as watch]
+            [rabble.resources.shared :as shared]
+            [rabble.db.validations :as validations]
+            [rabble.ring-app :as ra])
+  (:use midje.sweet
+        rabble.paths
+        rabble.test.resource-helpers
+        rabble.test.db-helpers))
+
+(setup-db-background)
+
+(defn watch
+  ([]
+     (watch (:id (auth))))
+  ([userid]
+     (dj/one [:watch/user userid])))
+
+(defn test-app
+  []
+  (resource-app "/watches"
+                watch/resource-decisions
+                {}
+                shared/default-decisions))
+
+(fact "creating a watch results in success"
+  (let [auth (auth "joebob")
+        response (app-req test-app :post "/watches" {:topic-id (topic-id) :user-id (:id auth)} auth)]
+    (:db/id (watch (:id auth))) =not=> nil?
+    response => (contains {:status 201})))
+
+(fact "deleting a watch as the creator results in success"
+  (let [response (app-req test-app :delete (str "/watches/" (:db/id (watch))) nil (auth "flyingmachine"))]
+    (watch) => nil
+    response => (contains {:status 204})))
+
